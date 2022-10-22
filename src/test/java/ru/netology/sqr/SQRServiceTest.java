8a3943b56cf87e336e91fdf03625baf0680dd7c6
Alexey Gorokhov <bring974@gmail.com>
2022-10-22 10:37:53 +0300

    Initial commit

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,18 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class SQRServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/square.csv")
+    public void totalOfSquares(int expected, int x, int y) {
+        SQRService service = new SQRService();
+
+        int actual = service.calcSQR(x, y);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
